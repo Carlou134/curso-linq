b86b57b0a11f1deff9e2864388cf8dd2a0cc4036
Carlos Vásquez Rodriguez <81259501+Carlou134@users.noreply.github.com>
2024-07-29 20:58:32 -0500

    Uso de LINQ

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿LinqQueries queries = new LinqQueries();
+PrintValues(queries.TodaColeccion());
+
+void PrintValues(IEnumerable<Book> listLibros)
+{
+    Console.WriteLine("{0, -60}, {1, 9}, {2, 11}\n", "Titulo", "N. Paginas", "Fecha publicacion");
+    foreach (var item in listLibros)
+    {
+        Console.WriteLine("{0, -60}, {1, 9}, {2, 11}", item.Title, item.PageCount, item.PublishedDate.ToShortDateString());
+    }
+}
